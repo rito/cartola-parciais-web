fb657d515f12dc7666f3add594c79cd1e92f7757
Geovanne Duarte <geovanne@devgeek.com.br>
2017-11-14 23:30:27 -0200

    Colocando jogadores no campinho

--- app/src/main/java/br/com/devgeek/cartolaparciais/fragment/EscalacaoFragment.java
+++ app/src/main/java/br/com/devgeek/cartolaparciais/fragment/EscalacaoFragment.java
@@ -6,8 +6,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.google.gson.Gson;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import br.com.devgeek.cartolaparciais.R;
+import br.com.devgeek.cartolaparciais.model.AtletasPontuados;
+import br.com.devgeek.cartolaparciais.model.TimeFavorito;
+import br.com.devgeek.cartolaparciais.util.ClubesUtil;
+import io.realm.Realm;
+
+import static br.com.devgeek.cartolaparciais.util.CartolaParciaisUtil.logErrorOnConsole;
+import static br.com.devgeek.cartolaparciais.util.CartolaParciaisUtil.parseAndSortAtletasPontuados;
 
 /**
  * Created by geovannefduarte
@@ -20,9 +34,32 @@ public class EscalacaoFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
 
+        Realm realm = null;
         View view = inflater.inflate(R.layout.fragment_escalacao, container, false);
 
+        try {
 
+            realm = Realm.getDefaultInstance();
+            List<AtletasPontuados> atletasPontuados = new ArrayList<>();
+            TimeFavorito timeFavorito = realm.copyFromRealm(realm.where(TimeFavorito.class).equalTo("timeDoUsuario", true).findFirst());
+            if (timeFavorito != null && timeFavorito.getAtletas() != null) {
+
+                atletasPontuados = parseAndSortAtletasPontuados(new Gson(), timeFavorito.getAtletas());
+
+                ImageView atacanteEsquerda = (ImageView) view.findViewById(R.id.atacanteEsquerda);
+                Picasso.with( getActivity() ).load( atletasPontuados.get(0).getFoto().replace("_FORMATO","_140x140") ).noFade().into( atacanteEsquerda );
+
+                ImageView atacanteEsquerdaEscudo = (ImageView) view.findViewById(R.id.atacanteEsquerdaEscudo);
+                atacanteEsquerdaEscudo.setImageResource(ClubesUtil.getClubeEscudo(atletasPontuados.get(0).getClubeId()));
+            }
+
+        } catch (Exception e){
+
+            logErrorOnConsole(TAG, "atletasPontuados do timeDoUsuario() -> "+e.getMessage(), e);
+
+        } finally {
+            if (realm != null) realm.close();
+        }
 
         return view;
     }
